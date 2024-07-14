64adab6c338256c56bfa943cd075879dfde930de
mikrokhvylovka777 <116650182+mikrokhvylovka777@users.noreply.github.com>
2024-07-14 18:23:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+    // Параметри слайдера
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+    },
+    // ... інші параметри
+  });
